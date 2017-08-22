5d42b8e3e35a9d5b52ec80e414fadaf5f1831e76
hezhuang321 <371718064@qq.com>
2017-08-22 10:01:19 +0800

    第一次更新

---
+++ src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Administrator on 2017/8/22 0022.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
